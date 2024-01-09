1b2d62266a34d720880af7a8d177adf296cfe59f
eugordinho <martinsanchezr@outlook.com>
2024-01-08 21:15:50 -0300

    Final changes

--- index.js
+++ index.js
@@ -41,3 +41,64 @@ $resetButton.addEventListener( "click", () => {
     $textContent.textContent = ""
     $textInput.value= ""
 })
+
+
+
+const $heightContainer = document.getElementById( 'heightContainer' )
+
+const $weightContainer = document.getElementById( 'weightContainer' )
+
+const $imcButton = document.getElementById( 'imcButton' )
+
+const $imcResultContainer = document.getElementById( 'imcResultContainer' )
+
+$imcButton.addEventListener( "click", () => {
+    const weight = Number( $weightContainer.value ) 
+
+    const height = Number( $heightContainer.value ) / 100
+
+    let imcResult = weight / Math.pow(height, 2) 
+
+    $imcResultContainer.textContent = imcResult.toFixed(1)
+
+    $weightContainer.value = ""
+
+    $heightContainer.value = ""
+
+}) 
+
+
+
+const $localCurrencyContainer = document.getElementById( 'localCurrencyContainer' )
+
+const $usdCurrencyResult = document.getElementById( 'usdCurrencyResult' )
+
+const $usdCurrencyContainer = document.getElementById( 'usdCurrencyContainer' )
+
+const $localCurrencyResult = document.getElementById( 'localCurrencyResult' )
+
+const $convertButton = document.getElementById( 'convertButton' )
+
+const $deleteButton = document.getElementById( 'deleteButton' )
+
+const $convertButton2 = document.getElementById( 'convertButton2' )
+
+const $deleteButton2 = document.getElementById( 'deleteButton2' )
+
+$convertButton.addEventListener( "click", () => $usdCurrencyResult.textContent = $localCurrencyContainer.value / 500)
+
+$convertButton2.addEventListener( "click", () => $localCurrencyResult.textContent = $usdCurrencyContainer.value * 500)
+
+$deleteButton.addEventListener( "click", () => {
+    $localCurrencyContainer.value = ""
+
+    $usdCurrencyResult.textContent = ""
+
+})
+
+$deleteButton2.addEventListener( "click", () => {
+    $usdCurrencyContainer.value = ""
+
+    $localCurrencyResult.textContent = ""
+
+})
